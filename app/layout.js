e341122426b6070f222cc4d7ca4338045d4e0c94
kasmil <kamilsalikhov75@gmail.com>
2026-09-08 14:19:55 +0300

    яндекс метрика

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import '@fontsource-variable/manrope';
 import '@fontsource-variable/unbounded';
@@ -8,5 +9,36 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="ru"><body>{children}</body></html>;
+  return (
+    <html lang="ru">
+      <body>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112384878', 'ym');
+
+              ym(112384878, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/112384878"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {children}
+      </body>
+    </html>
+  );
 }
